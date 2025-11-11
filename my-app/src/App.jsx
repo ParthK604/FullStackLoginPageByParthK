bd33cea8d9481ec64e56518ea337603217ca4894
ParthK604 <parthkamath343@gmail.com>
2025-11-12 01:32:49 +0530

    final commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
 
   const onSubmit = async (data) => {
     try {
-      const res = await fetch("http://localhost:3000/", {
+      const res = await fetch("https://signuppagebyparthkamath.onrender.com/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
